45a66eac06cd9176b717df0277604cf9dc574d87
Arpit Sheth <shetharp@gmail.com>
2020-07-13 03:18:00 -0400

    Add react-headroom and configure pinned header behavior

--- theme/src/components/header.tsx
+++ theme/src/components/header.tsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import React from "react";
+import React, { useState } from "react";
 import { jsx, useColorMode } from "theme-ui";
 import { Flex } from "@theme-ui/components";
 import useMinimalBlogConfig from "../hooks/use-minimal-blog-config";
@@ -7,31 +7,52 @@ import ColorModeToggle from "./colormode-toggle";
 import Navigation from "./navigation";
 import HeaderTitle from "./header-title";
 import HeaderExternalLinks from "./header-external-links";
+import Headroom from "react-headroom";
 
 export type HeaderProps = {
-  // empty
+  isTransparent?: boolean;
 };
 
-export const Header: React.FC<HeaderProps> = () => {
+export const Header: React.FC<HeaderProps> = (props) => {
   const { navigation: nav } = useMinimalBlogConfig();
   const [colorMode, setColorMode] = useColorMode();
+  const [isVisible, setIsVisible] = useState(true);
+  const [isFixed, setIsFixed] = useState(false);
   const isDark = colorMode === `dark`;
+
   const toggleColorMode = (e: React.MouseEvent) => {
     e.preventDefault();
     setColorMode(isDark ? `light` : `dark`);
   };
 
   return (
-    <header sx={sxHeader}>
-      <Flex sx={sxFlex}>
-        <HeaderTitle />
-        <ColorModeToggle isDark={isDark} toggle={toggleColorMode} />
-      </Flex>
-      <div sx={sxDiv}>
-        <Navigation nav={nav} />
-        <HeaderExternalLinks />
-      </div>
-    </header>
+    <Headroom
+      onPin={() => {
+        setIsVisible(true);
+        setIsFixed(true);
+      }}
+      onUnpin={() => {
+        setIsVisible(false);
+      }}
+      onUnfix={() => {
+        setIsFixed(false);
+      }}
+      wrapperStyle={{
+        position: props.isTransparent ? "absolute" : "relative",
+        width: "100%",
+      }}
+    >
+      <header sx={sxHeader(isVisible, isFixed, props.isTransparent || false)}>
+        <Flex sx={sxFlex}>
+          <HeaderTitle />
+          <ColorModeToggle isDark={isDark} toggle={toggleColorMode} />
+        </Flex>
+        <div sx={sxDiv}>
+          <Navigation nav={nav} />
+          <HeaderExternalLinks />
+        </div>
+      </header>
+    </Headroom>
   );
 };
 export default Header;
@@ -40,10 +61,12 @@ export default Header;
  * Styles
  */
 
-const sxHeader = {
+const sxHeader = (isVisible: boolean, isFixed: boolean, isTransparent: boolean) => ({
   outline: `4px solid mediumslateblue`, // TODO Remove
-  mb: [8, null, null, null, 9, null, null, 10],
-};
+  backgroundColor: !isFixed && isTransparent ? `transparent` : `background`,
+  opacity: isVisible ? 1 : 0,
+  transition: `all 300ms ease`,
+});
 
 const sxFlex = { alignItems: `center`, justifyContent: `space-between` };
 
