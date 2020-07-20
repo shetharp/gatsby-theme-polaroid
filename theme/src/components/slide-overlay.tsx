8467dac9ea6750e2fbaa3d35c274090a8a0b468a
Arpit Sheth <shetharp@gmail.com>
2020-07-20 13:02:10 -0400

    Style slide overlay

--- theme/src/components/slide-overlay.tsx
+++ theme/src/components/slide-overlay.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Box } from "theme-ui";
+import { useThemeUI, Theme, Box } from "theme-ui";
+import { alpha } from "@theme-ui/color";
 import { SystemStyleObject } from "@styled-system/css";
+import { ThemePolaroid } from "../gatsby-plugin-theme-ui";
 
 export type SlideOverlayProps = {
   overlayColor: string;
@@ -9,20 +11,44 @@ export type SlideOverlayProps = {
 
 export const SlideOverlay: React.FC<SlideOverlayProps> = (props) => {
   const { overlayColor, isColorful } = props;
-  return <Box sx={sxOverlay(overlayColor, isColorful)} />;
+  const { theme } = useThemeUI();
+
+  return <Box sx={sxOverlay(overlayColor, isColorful, theme as ThemePolaroid)} />;
 };
 
 /**
  * Styles
  */
-const sxOverlay = (overlayColor: string, isColorful: boolean): SystemStyleObject => {
+
+const sxOverlay = (overlayColor: string, isColorful: boolean, theme: ThemePolaroid): SystemStyleObject => {
+  // Generate gradients from the overlay color
+  // https://theme-ui.com/packages/color#advanced-usage
+  const gradientColorfulMobile = `linear-gradient(185deg, ${alpha(overlayColor, 0.4)(theme)} 0%, ${alpha(
+    overlayColor,
+    0.8
+  )(theme)} 50%, ${alpha(overlayColor, 0.9)(theme)} 100%)`;
+  const gradientMobile = `linear-gradient(185deg, ${alpha(overlayColor, 0.0)(theme)} 0%, ${alpha(
+    overlayColor,
+    0.6
+  )(theme)} 50%, ${alpha(overlayColor, 0.9)(theme)} 100%)`;
+  const gradientColorfulLaptop = `linear-gradient(190deg, ${alpha(overlayColor, 0.3)(theme)} 0%, ${alpha(
+    overlayColor,
+    0.6
+  )(theme)} 50%, ${alpha(overlayColor, 0.9)(theme)} 100%)`;
+  const gradientLaptop = `linear-gradient(190deg, ${alpha(overlayColor, 0.0)(theme)} 0%, ${alpha(
+    overlayColor,
+    0.4
+  )(theme)} 50%, ${alpha(overlayColor, 0.6)(theme)} 100%)`;
+
   return {
     position: "absolute",
     top: 0,
     left: 0,
     width: "100%",
     height: "100%",
-    background: overlayColor,
-    opacity: 0.5,
+    backgroundImage: isColorful ? gradientColorfulMobile : gradientMobile,
+    [`@media ${theme.mediaQuery.laptopS}`]: {
+      backgroundImage: isColorful ? gradientColorfulLaptop : gradientLaptop,
+    },
   };
 };
