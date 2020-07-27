f53cda6ca16c7678c3bf7beb634808f05bd0f11d
Arpit Sheth <shetharp@gmail.com>
2020-07-27 01:07:28 -0400

    Improve Tags page styles

--- theme/src/components/tags.tsx
+++ theme/src/components/tags.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /** @jsx jsx */
-import { jsx, Link as TLink, Heading } from "theme-ui";
-import { Box, Flex } from "@theme-ui/components";
+import { jsx, Box, Flex, Link as TLink, Heading, Divider, Text } from "theme-ui";
 import kebabCase from "lodash.kebabcase";
 import { Link } from "gatsby";
 import Layout from "./layout";
 import { useMinimalBlogConfig } from "../hooks";
 import SEO from "./seo";
 import replaceSlashes from "../utils/replaceSlashes";
+import { SystemStyleObject } from "@styled-system/css";
 
 export type PostsProps = {
   list: {
@@ -23,23 +23,57 @@ export const Tags = ({ list }: PostsProps) => {
   return (
     <Layout>
       <SEO title="Tags" />
-      <Heading variant="styles.h2">Tags</Heading>
-      <Box mt={[4, 5]}>
-        {list.map((listItem) => (
-          <Flex key={listItem.fieldValue} mb={[1, 1, 2]} sx={{ alignItems: `center` }}>
-            <TLink
-              as={Link}
-              sx={{ variant: `links.listItem`, mr: 2 }}
-              // @ts-ignore
-              to={replaceSlashes(`/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`)}
-            >
-              {listItem.fieldValue} <span sx={{ color: `secondary` }}>({listItem.totalCount})</span>
-            </TLink>
-          </Flex>
-        ))}
+      <Heading>Tags</Heading>
+
+      <Divider variant="dividers.pageHeading" />
+
+      <Box>
+        <ul>
+          {list.map((listItem) => (
+            <li key={listItem.fieldValue}>
+              <Flex>
+                <TLink
+                  as={Link}
+                  // @ts-ignore
+                  to={replaceSlashes(`/${basePath}/${tagsPath}/${kebabCase(listItem.fieldValue)}`)}
+                  sx={sxTagTitleLink}
+                >
+                  <Text sx={sxTagTitle}>{listItem.fieldValue}</Text>
+                </TLink>
+                <Text variant="mono" color="accent">
+                  ({listItem.totalCount})
+                </Text>
+              </Flex>
+            </li>
+          ))}
+        </ul>
       </Box>
     </Layout>
   );
 };
 
 export default Tags;
+
+/**
+ * Styles
+ */
+const sxTagListItem: SystemStyleObject = {
+  display: "flex",
+};
+
+const sxTagTitleLink: SystemStyleObject = {
+  color: "inherit",
+  textDecoration: "none",
+  [":hover"]: {
+    textDecoration: "underline",
+  },
+};
+
+const sxTagTitle: SystemStyleObject = {
+  color: "inherit",
+  lineHeight: "heading",
+  fontWeight: "heading",
+  fontSize: [1, null, null, 2, null, null, 3, null, 4], // Size up from SlideDescription fontSize
+  marginRight: 1,
+  marginBottom: 3,
+};
