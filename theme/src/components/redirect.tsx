68b93b32b76830b8562c6d421c82d11efcd1f8ed
Arpit Sheth <shetharp@gmail.com>
2020-07-31 00:03:34 -0400

    Remove delay on Redirect

--- theme/src/components/redirect.tsx
+++ theme/src/components/redirect.tsx
@@ -7,7 +7,7 @@ export type RedirectProps = {
 };
 
 export const Redirect: React.FC<RedirectProps> = (props) => {
-  const { to, delay = 1000, shouldRender = true } = props;
+  const { to, delay = 0, shouldRender = true } = props;
   const setRedirect = () => {
     window.location.href = to;
   };
